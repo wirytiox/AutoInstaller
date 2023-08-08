48a99dbe0edfde5bb125798938fccb8dc6595224
wirytiox <79142532+wirytiox@users.noreply.github.com>
2023-08-08 20:47:35 -0300

    modified:   AutoInstaller.bat

--- AutoInstaller.bat
+++ AutoInstaller.bat
@@ -1,5 +1,4 @@
 @echo off
-
 choco -v >nul 2>&1
 if %errorlevel% equ 0 (
     echo Chocolatey is already installed on this system.
@@ -10,13 +9,11 @@ if %errorlevel% equ 0 (
     powershell.exe "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
     echo Restart the Powershell and execute it with administrator privileges
 )
-echo
 curl --version > nul 2>&1
 if %errorlevel% neq 0 (
     echo curl is not installed. Installing curl...
     powershell.exe -Command "choco install curl -y"
 )
-
 set /p install_obs=Do you want to install OBS Studio? (Y/N): 
 if /i "%install_obs%"=="Y" (
     powershell.exe -Command "choco install obs-studio -y"
@@ -24,7 +21,6 @@ if /i "%install_obs%"=="Y" (
 ) else (
     echo Skipping OBS Studio installation.
 )
-
 set /p install_lghub=Do you want to install Logitech G Hub? (Y/N): 
 if /i "%install_lghub%"=="Y" (
     powershell.exe -Command "choco install lghub -y"
@@ -32,7 +28,6 @@ if /i "%install_lghub%"=="Y" (
 ) else (
     echo Skipping Logitech G Hub installation.
 )
-
 echo Do you want to install Visual Studio Code with Chocolatey? (Y/N)
 set /p choice=
 if /i "%choice%"=="Y" (
@@ -41,7 +36,6 @@ if /i "%choice%"=="Y" (
 ) else (
     echo Skipping installation of Visual Studio Code.
 )
-
 echo Do you want to install Notepad++ with Chocolatey? (Y/N)
 set /p choice=
 if /i "%choice%"=="Y" (
@@ -50,8 +44,8 @@ if /i "%choice%"=="Y" (
 ) else (
     echo Skipping installation of Notepad++.
 )
-
-echo YOU'RE GONNA GET HWINFO AND YOU WILL LOVE IT!!!
+echo Installing Must programs...
+echo Installing HWINFO
 powershell.exe -Command "choco install hwinfo -y"
 echo Installing latest JDK version.
 powershell.exe -Command "choco install openjdk -y"
@@ -59,10 +53,7 @@ echo Installing latest Python version.
 powershell.exe -Command "choco install python -y"
 echo Installing latest Nmap version.
 powershell.exe -Command "choco install nmap -y"
-
-
 wmic path win32_videocontroller get caption | find /i "NVIDIA" > nul
-
 if %ERRORLEVEL% equ 0 (
     echo NVIDIA GPU detected.
     start "" "https://la.nvidia.com/Download/index.aspx?lang=la"
